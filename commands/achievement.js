7c3acc93b9f1377faed2b66a6a27ee1dffb8d823
Ryan Cannon <kainryn@gmail.com>
2017-12-19 00:19:43 +0000

    added mentions to achievement command and reverted error handling to show trace again

--- commands/achievement.js
+++ commands/achievement.js
@@ -1,7 +1,8 @@
 const snekfetch = require('snekfetch');
 
 exports.run = (client, message, args) => {
-  let [title, contents] = args.join(" ").split("|");
+  let [title, contents] = args.filter((a) => a.indexOf("<@") < 0).join(" ").split("|");
+  client.logger.debug(`${title}, ${contents}`);
   if(!contents) {
     [title, contents] = ["Achievement Get!", title];
   }
@@ -9,13 +10,16 @@ exports.run = (client, message, args) => {
   if(args.join(" ").toLowerCase().includes("burn")) rnd = 38;
   if(args.join(" ").toLowerCase().includes("cookie")) rnd = 21;
   if(args.join(" ").toLowerCase().includes("cake")) rnd = 10;
+  
+  let users = message.mentions.users;
 
-  if(title.length > 22 || contents.length > 22) return message.edit("Max Length: 22 Characters. Soz.").then(message.delete.bind(message), 2000);
+  if(title.length > 22 || contents.length > 22) return message.reply("Achievement title and content cannot exceed 22 characters each.").then(message.delete.bind(message), 3000);
   const url = `https://www.minecraftskinstealer.com/achievement/a.php?i=${rnd}&h=${encodeURIComponent(title)}&t=${encodeURIComponent(contents)}`;
-  snekfetch.get(url).then(r=>message.channel.send("", {files:[{attachment: r.body}]}));
-  if(!!message.guild) {
-    message.delete().catch(console.error);
-  }
+  snekfetch.get(url).then(r=>message.channel.send(users.array().join(", "), {files:[{attachment: r.body}]})).then(() => {
+    if(!!message.guild && message.deletable) {
+      message.delete();
+    }
+  });
 };
 
 exports.conf = {
--- modules/functions.js
+++ modules/functions.js
@@ -139,13 +139,14 @@ module.exports = (client) => {
   // These 2 process methods will catch exceptions and give *more details* about the error and stack trace.
   process.on("uncaughtException", (err) => {
     const errorMsg = err.stack.replace(new RegExp(`${__dirname}/`, "g"), "./");
-    client.logger.error(`Uncaught Exception: ${errorMsg}`);
+    client.logger.error("Uncaught Exception: " + errorMsg);
     // Always best practice to let the code crash on uncaught exceptions. 
     // Because you should be catching them anyway.
     process.exit(1);
   });
 
   process.on("unhandledRejection", err => {
-    client.logger.error(`Unhandled rejection: ${err}`);
+    const errorMsg = err.stack.replace(new RegExp(`${__dirname}/`, "g"), "./");
+    client.logger.error("Unhandled rejection: " + errorMsg);
   });
 };
