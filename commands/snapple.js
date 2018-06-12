ca3e8cad7ffd7b88a4c8b11c138609416e96cc52
Ryan Cannon <kainryn@gmail.com>
2018-06-12 21:12:00 +0000

    added snapple command using cheerio for dom parsing

---
+++ commands/snapple.js
@@ -0,0 +1,27 @@
+const snekfetch = require("snekfetch");
+const cheerio = require("cheerio");
+const url = "https://www.snapple.com/real-facts";
+
+exports.run = (client, message, args, level) => {
+  snekfetch.get(url).then((r) => {
+    let $ = cheerio.load(r.body);
+    let num = +$("#facts .cap").data("fact");
+    let desc = $("#facts .fact-description").first().text().trim();
+    
+    message.reply(`Real Fact #${num}: ${desc}`);
+  });
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ["snapfact"],
+  permLevel: "User"
+};
+
+exports.help = {
+  name: "snapple",
+  category: "Fun",
+  description: "Gets a random snapple fact.",
+  usage: "\n  snapple"
+};
