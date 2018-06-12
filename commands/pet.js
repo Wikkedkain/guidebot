aac4cfa03849ab5ae6c8d18b22871afdd4eaca37
Ryan Cannon <kainryn@gmail.com>
2018-06-12 21:00:39 +0000

    added silly pet command

---
+++ commands/pet.js
@@ -0,0 +1,43 @@
+let actions = ["Squeezes", "Pets", "Hugs", "Kisses"];
+let pets = ["Hamster", "Kitty", "Doggy"];
+let petsPlural = ["Hamsters", "Kitties", "Doggies"];
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+exports.run = (client, message, args, level) => {
+  let users = message.mentions.users;
+  
+  let action = actions[getRandomInt(0, actions.length)];
+  
+  let index = getRandomInt(0, pets.length);
+  
+  let pet = users.size > 1 ? petsPlural[index] : pets[index];
+  
+  if(users.size < 1) return message.channel.send(`\\*${action} ${message.author}*. Good ${pet}.`).then(() => {
+    if(!!message.guild && message.deletable) {
+      message.delete().catch(client.logger.error);
+    }
+  });
+  
+  return message.channel.send(`\\*${action} ${users.array().join(', ')}*. Good ${pet}.`).then(() => {
+    if(!!message.guild && message.deletable) {
+      message.delete().catch(client.logger.error);
+    }
+  });
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: "User"
+};
+
+exports.help = {
+  name: "pet",
+  category: "Fun",
+  description: "Pets the user(s) mentioned (or yourself if no users are mentioned)",
+  usage: "\n  pet <@user>"
+};
