9c3a346e55b004e4ce3ec3351fab926060507b05
Ryan Cannon <kainryn@gmail.com>
2020-01-08 00:14:04 +0000

    Dice command now supports rolling more than one die at a time, with ordering and sum to aid comprehension.

--- commands/dice.js
+++ commands/dice.js
@@ -1,10 +1,26 @@
+const rollDice = ((sides) => Math.floor(Math.random() * sides) + 1);
 
 exports.run = (client, message, args, level) => {
-    const n = args[0];
-    if (isNaN(n)) return message.reply("You must specify a number to roll a dice.").catch(console.error);
-    let roll = ((sides) => Math.floor(Math.random() * sides) + 1)(n);
+    client.logger.debug(message, args);
+    const x = args.length > 1 ? args[0] : 1; // if 2 arguments, first one is number of dice
+    const n = args.length > 1 ? args[1] : args[0]; // if 2 arguments, second one is number of dice; otherwise first argument
     
-    message.reply(`You rolled a ${roll}!`);
+    if (isNaN(x) || isNaN(n)) {
+        return message.reply("You must specify a number to roll a dice.").catch(err => client.logger.error(err));
+    }
+    
+    const rolls = [];
+    for(let i = 0; i < x; i++) {
+        rolls.push(rollDice(n));
+    }
+    
+    if(rolls.length === 1) {
+        message.reply(`You rolled a ${rolls[0]}!`).catch(err => client.logger.error(err));
+    } else {
+        let rollText = rolls.sort((a,b) => b-a).map((r,i) => `\n${r}`);
+        let sum = rolls.reduce((a,b) => a+b);
+        message.reply(`You rolled:\n${rollText.join('')}\nSum: ${sum}`).catch(err => client.logger.error(err));
+    }
 };
 
 exports.conf = {
@@ -17,6 +33,6 @@ exports.conf = {
 exports.help = {
     name: "dice",
     category: "Miscellaneous",
-    description: "Rolls a dice of n sides.",
-    usage: "dice <n>"
+    description: "Rolls a dice of n sides. If 2 given two numbers, the first becomes the number of dice to roll and the second is the number of sides.",
+    usage: "dice <sides>\n\ndice <quantity> <sides>"
 };
