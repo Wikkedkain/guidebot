af9df7bf1685c96ee6ad25ddcadfefa8912996c8
Ryan Cannon <ryan.cannon@ircoder.com>
2021-06-01 02:02:51 -0400

    Updating postgres and setting ssl for connection string in prod

--- modules/db.js
+++ modules/db.js
@@ -1,11 +1,12 @@
-const { Pool } = require("pg");
+const { Pool } = require('pg');
 
 const pool = createPool();
 
 function createPool() {
-  if(process.env.DATABASE_URL != undefined) {
+  if (process.env.DATABASE_URL != undefined) {
+    let ssl = process.env.NODE_ENV == 'production' ? '?ssl=require' : '';
     return new Pool({
-      connectionString: process.env.DATABASE_URL
+      connectionString: process.env.DATABASE_URL + ssl,
     });
   }
   return new Pool();
@@ -13,4 +14,4 @@ function createPool() {
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
-}
+};
